bc6780774bdf780cc7025b942a07eccdc89479df
Maksim Belov <maksimbelov95@mail.ru>
2023-09-14 21:50:15 +0300

    спринт 10

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+    let AccessKey = "UZ45hIGtRsVO2y0XXnBx_pB8PGhp4fGvHLt6cxeL3BM"
+    let SecretKey = "szSRnbtDHe3fGcLkdgB3tmkqEprP9RhQ5naONjuGVvk"
+    let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let AccessScope = "public+read_user+write_likes"
+
+
