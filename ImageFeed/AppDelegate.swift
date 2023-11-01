13cea855cc527357b2f574be2e2e286f4552d779
Maksim Belov <maksimbelov95@mail.ru>
2023-11-01 20:38:33 +0300

    no message

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -13,11 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-//        ProgressHUD.animationType = .activityIndicator
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+//        ProgressHUD.animationType = .systemActivityIndicator
 //        ProgressHUD.colorHUD = .black
 //        ProgressHUD.colorAnimation = .lightGray
         return true
